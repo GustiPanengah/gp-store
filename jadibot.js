6b83d4a333cb80fa7442f2b33a2be97b57135346
krishosting <111237854+krishosting569@users.noreply.github.com>
2022-11-13 10:41:36 +0700

    Update jadibot.js

--- jadibot.js
+++ jadibot.js
@@ -1,6 +1,5 @@
 const { modul } = require('./module');
 const { baileys, boom, chalk, fs, figlet, FileType, path, process, PhoneNumber } = modul;
-const { Boom } = boom
 const { default: makeWaSocket, useMultiFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, generateWAMessage, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = baileys
 const { color, bgcolor } = require('./lib/color')
 const log = (pino = require("pino"));
@@ -13,70 +12,70 @@ const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream
 if (global.conns instanceof Array) console.log()
 else global.conns = []
 
-const jadibot = async (baymax, m, from) => {
-const { sendImage, sendMessage } = baymax;
+const jadibot = async (kris, m, from) => {
+const { sendImage, sendMessage } = kris;
 const { reply, sender } = m;
 const { state, saveCreds } = await useMultiFileAuthState(path.join(__dirname, `./database/jadibot/session-${sender.split("@")[0]}`), log({ level: "silent" }));
 try {
 async function start() {
 let { version, isLatest } = await fetchLatestBaileysVersion();
-const baymax = await makeWaSocket({
+const kris = await makeWaSocket({
 auth: state,
-browser: [`Jadibot Md By (baymax Bot)`, "Chrome", "1.0.0"],
+browser: [`Jadibot Md By (kris Bot)`, "Chrome", "1.0.0"],
 logger: log({ level: "silent" }),
 version,
 })
 
-baymax.ev.on('messages.upsert', async chatUpdate => {
+kris.ev.on('messages.upsert', async chatUpdate => {
 try {
 kay = chatUpdate.messages[0]
 if (!kay.message) return
 kay.message = (Object.keys(kay.message)[0] === 'ephemeralMessage') ? kay.message.ephemeralMessage.message : kay.message
 if (kay.key && kay.key.remoteJid === 'status@broadcast') return
-if (!baymax.public && !kay.key.fromMe && chatUpdate.type === 'notify') return
+if (!kris.public && !kay.key.fromMe && chatUpdate.type === 'notify') return
 if (kay.key.id.startsWith('BAE5') && kay.key.id.length === 16) return
-m = smsg(baymax, kay, store)
-require('./baymax')(baymax, m, chatUpdate, store)
+m = smsg(kris, kay, store)
+require('./kris')(kris, m, chatUpdate, store)
 } catch (err) {
 console.log(err)}
 })
 
-baymax.public = true
+kris.public = true
 
-store.bind(baymax.ev);
-baymax.ev.on("creds.update", saveCreds);
-baymax.ev.on("connection.update", async up => {
+store.bind(kris.ev);
+kris.ev.on("creds.update", saveCreds);
+kris.ev.on("connection.update", async up => {
 const { lastDisconnect, connection } = up;
 if (connection == "connecting") return
 if (connection){
 if (connection != "connecting") console.log("Connecting to jadibot..")
 }
 console.log(up)
-if (up.qr) await sendImage(from, await qrcode.toDataURL(up.qr,{scale : 8}), 'Scan QR ini untuk jadi bot sementara\n\n1. Klik titik tiga di pojok kanan atas\n2. Ketuk WhatsApp Web By BayMax\n3. Scan QR ini \nQR Expired dalam 20 detik', m)
+if (up.qr) await sendImage(from, await qrcode.toDataURL(up.qr,{scale : 8}), 'Scan QR ini untuk jadi bot sementara\n\n1. Klik titik tiga di pojok kanan atas\n2. Ketuk WhatsApp Web By kris\n3. Scan QR ini \nQR Expired dalam 20 detik', m)
 console.log(connection)
 if (connection == "open") {
-baymax.id = baymax.decodeJid(baymax.user.id)
-baymax.time = Date.now()
-global.conns.push(baymax)
-await m.reply(`*Connected to Whatsapp - Bot*\n\n*User :*\n _*× id : ${baymax.decodeJid(baymax.user.id)}*_`)
-user = `${baymax.decodeJid(baymax.user.id)}`
+kris.id = kris.decodeJid(kris.user.id)
+kris.time = Date.now()
+global.conns.push(kris)
+await m.reply(`*Connected to Whatsapp - Bot*\n\n*User :*\n _*× id : ${kris.decodeJid(kris.user.id)}*_`)
+user = `${kris.decodeJid(kris.user.id)}`
 txt = `*Terdeteksi menumpang Jadibot*\n\n _× User : @${user.split("@")[0]}_`
 sendMessage(`6281390368580@s.whatsapp.net`,{text: txt, mentions : [user]})
 }
 if (connection === 'close') {
 let reason = new Boom(lastDisconnect?.error)?.output.statusCode
-if (reason === DisconnectReason.badSession) { console.log(`Bad Session File, Please Delete Session and Scan Again`); baymax.logout(); }
+if (reason === DisconnectReason.badSession) { console.log(`Bad Session File, Please Delete Session and Scan Again`); kris.logout(); }
 else if (reason === DisconnectReason.connectionClosed) { console.log("Connection closed, reconnecting...."); start(); }
 else if (reason === DisconnectReason.connectionLost) { console.log("Connection Lost from Server, reconnecting..."); start(); }
-else if (reason === DisconnectReason.connectionReplaced) { console.log("Connection Replaced, Another New Session Opened, Please Close Current Session First"); baymax.logout(); }
-else if (reason === DisconnectReason.loggedOut) { console.log(`Device Logged Out, Please Scan Again And Run.`); baymax.logout(); }
+else if (reason === DisconnectReason.connectionReplaced) { console.log("Connection Replaced, Another New Session Opened, Please Close Current Session First"); kris.logout(); }
+else if (reason === DisconnectReason.loggedOut) { console.log(`Device Logged Out, Please Scan Again And Run.`); kris.logout(); }
 else if (reason === DisconnectReason.restartRequired) { console.log("Restart Required, Restarting..."); start(); }
 else if (reason === DisconnectReason.timedOut) { console.log("Connection TimedOut, Reconnecting..."); start(); }
-else baymax.end(`Unknown DisconnectReason: ${reason}|${connection}`)
+else kris.end(`Unknown DisconnectReason: ${reason}|${connection}`)
 }
 })
 
-baymax.decodeJid = (jid) => {
+kris.decodeJid = (jid) => {
 if (!jid) return jid
 if (/:\d+@/gi.test(jid)) {
 let decode = jidDecode(jid) || {}
@@ -84,32 +83,32 @@ return decode.user && decode.server && decode.user + '@' + decode.server || jid
 } else return jid
 }
 
-baymax.ev.on('contacts.update', update => {
+kris.ev.on('contacts.update', update => {
 for (let contact of update) {
-let id = baymax.decodeJid(contact.id)
+let id = kris.decodeJid(contact.id)
 if (store && store.contacts) store.contacts[id] = { id, name: contact.notify }
 }
 })
 
-baymax.getName = (jid, withoutContact  = false) => {
-id = baymax.decodeJid(jid)
-withoutContact = baymax.withoutContact || withoutContact 
+kris.getName = (jid, withoutContact  = false) => {
+id = kris.decodeJid(jid)
+withoutContact = kris.withoutContact || withoutContact 
 let v
 if (id.endsWith("@g.us")) return new Promise(async (resolve) => {
 v = store.contacts[id] || {}
-if (!(v.name || v.subject)) v = baymax.groupMetadata(id) || {}
+if (!(v.name || v.subject)) v = kris.groupMetadata(id) || {}
 resolve(v.name || v.subject || PhoneNumber('+' + id.replace('@s.whatsapp.net', '')).getNumber('international'))
 })
 else v = id === '0@s.whatsapp.net' ? {
 id,
 name: 'WhatsApp'
-} : id === baymax.decodeJid(baymax.user.id) ?
-baymax.user :
+} : id === kris.decodeJid(kris.user.id) ?
+kris.user :
 (store.contacts[id] || {})
 return (withoutContact ? '' : v.name) || v.subject || v.verifiedName || PhoneNumber('+' + jid.replace('@s.whatsapp.net', '')).getNumber('international')
 }
 
-baymax.sendText = (jid, text, quoted = '', options) => baymax.sendMessage(jid, { text: text, ...options }, { quoted })
+kris.sendText = (jid, text, quoted = '', options) => kris.sendMessage(jid, { text: text, ...options }, { quoted })
 
 }
 start()
