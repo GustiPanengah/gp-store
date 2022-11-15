dcf2145d6c64226a6efa2e8df78591f01c6d7b89
krishosting <111237854+krishosting569@users.noreply.github.com>
2022-11-15 16:42:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-/* Created By kris Bot WhatsApp */
+/* Created By GpStore Bot WhatsApp */
 /* WhatsApp Creator Di Bawah */
 /* wa.me/6287705048235 */
 
 const { modul } = require('./module');
 const { baileys, boom, chalk, fs, figlet, FileType, path, pino, process, PhoneNumber } = modul;
-const { default: krisConnect, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, generateWAMessage, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = baileys
+const { default: GpStoreConnect, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, generateWAMessage, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = baileys
 const { color, bgcolor } = require('./lib/color')
 const { uncache, nocache } = require('./lib/loader')
 const { state } = useSingleFileAuthState(`./kayla.json`)
@@ -15,41 +15,41 @@ const owner = JSON.parse(fs.readFileSync('./database/owner.json'))
 
 const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
 
-require('./kris.js')
-nocache('../kris.js', module => console.log(color('[ UPDATE ]', 'cyan'), color(`'${module}'`, 'green'), 'File Is Update!!!'))
+require('./GpStore.js')
+nocache('../GpStore.js', module => console.log(color('[ UPDATE ]', 'cyan'), color(`'${module}'`, 'green'), 'File Is Update!!!'))
 require('./index.js')
 nocache('../index.js', module => console.log(color('[ UPDATE ]', 'cyan'), color(`'${module}'`, 'green'), 'File Is Update!!!'))
 
-async function krisBot() {
+async function GpStoreBot() {
 const { version, isLatest } = await fetchLatestBaileysVersion()
-const kris = krisConnect({
+const GpStore = GpStoreConnect({
 logger: pino({ level: 'silent' }),
 printQRInTerminal: true,
-browser: ['kris Bot WhatsApp (2022)','Safari','1.0.0'],
+browser: ['GpStore Bot WhatsApp (2022)','Safari','1.0.0'],
 auth: state,
 version
 })
 
-store.bind(kris.ev)
+store.bind(GpStore.ev)
 
-console.log(color(figlet.textSync(`kris`, {
+console.log(color(figlet.textSync(`GpStore`, {
 font: 'Standard',
 horizontalLayout: 'default',
 vertivalLayout: 'default',
 whitespaceBreak: false
 }), 'green'))
 
-kris.ws.on('CB:Blocklist', json => {
+GpStore.ws.on('CB:Blocklist', json => {
 if (blocked.length > 2) return
 for (let i of json[1].blocklist) {
 blocked.push(i.replace('c.us','s.whatsapp.net'))}})
 
-kris.ws.on('CB:call', async (json) => {
+GpStore.ws.on('CB:call', async (json) => {
 const callerId = json.content[0].attrs['call-creator']
 const idCall = json.content[0].attrs['call-id']
 const Id = json.attrs.id
 const T = json.attrs.t
-kris.sendNode({
+GpStore.sendNode({
   tag: 'call',
     attrs: {
       from: '6285798145596@s.whatsapp.net',
@@ -69,45 +69,45 @@ kris.sendNode({
     ]
 })
 if (json.content[0].tag == 'offer') {
-let qutsnya = await kris.sendContact(callerId, owner)
-await kris.sendMessage(callerId, { text: `Sistem Otomatis Block!!!\nJangan Menelpon Bot!!!\nSilahkan Hubungi Owner Untuk Dibuka!!!`}, { quoted : qutsnya })
+let qutsnya = await GpStore.sendContact(callerId, owner)
+await GpStore.sendMessage(callerId, { text: `Sistem Otomatis Block!!!\nJangan Menelpon Bot!!!\nSilahkan Hubungi Owner Untuk Dibuka!!!`}, { quoted : qutsnya })
 await sleep(8000)
-await kris.updateBlockStatus(callerId, "block")
+await GpStore.updateBlockStatus(callerId, "block")
 }
 })
 
-kris.ev.on('messages.upsert', async chatUpdate => {
+GpStore.ev.on('messages.upsert', async chatUpdate => {
 try {
 kay = chatUpdate.messages[0]
 if (!kay.message) return
 kay.message = (Object.keys(kay.message)[0] === 'ephemeralMessage') ? kay.message.ephemeralMessage.message : kay.message
 if (kay.key && kay.key.remoteJid === 'status@broadcast') return
-if (!kris.public && !kay.key.fromMe && chatUpdate.type === 'notify') return
+if (!GpStore.public && !kay.key.fromMe && chatUpdate.type === 'notify') return
 if (kay.key.id.startsWith('BAE5') && kay.key.id.length === 16) return
-m = smsg(kris, kay, store)
-require('./kris')(kris, m, chatUpdate, store)
+m = smsg(GpStore, kay, store)
+require('./GpStore')(GpStore, m, chatUpdate, store)
 } catch (err) {
 console.log(err)}})
 
-kris.ev.on('group-participants.update', async (anu) => {
+GpStore.ev.on('group-participants.update', async (anu) => {
 console.log(anu)
 try {
-let metadata = await kris.groupMetadata(anu.id)
+let metadata = await GpStore.groupMetadata(anu.id)
 let participants = anu.participants
 let memb = metadata.participants.length
 for (let num of participants) {
 try {
-ppowner = await kris.profilePictureUrl(num, 'image')
+ppowner = await GpStore.profilePictureUrl(num, 'image')
 } catch {
 ppowner = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 }
 try {
-ppgroup = await kris.profilePictureUrl(anu.id, 'image')
+ppgroup = await GpStore.profilePictureUrl(anu.id, 'image')
 } catch {
 ppgroup = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 }
 if (anu.action == 'add') {
-kris.sendMessage(anu.id, { image: { url: ppowner }, contextInfo: { mentionedJid: [num] }, caption: `Hello Sis @${num.split("@")[0]}, Welcome To The Group ${metadata.subject}, I Hope You Enjoy It, Sis`, 
+GpStore.sendMessage(anu.id, { image: { url: ppowner }, contextInfo: { mentionedJid: [num] }, caption: `Hello Sis @${num.split("@")[0]}, Welcome To The Group ${metadata.subject}, I Hope You Enjoy It, Sis`, 
 jpegThumbnail: await reSize(ppowner, 200, 200), 
 contextInfo: {
 "mentionedJid": [num],
@@ -122,7 +122,7 @@ contextInfo: {
 "sourceUrl": 'https://telegra.ph/file/886bef2ade7c759c377f6.jpg'
 }}})
 } else if (anu.action == 'remove') {
-kris.sendMessage(anu.id, { image: { url: ppowner }, contextInfo: { mentionedJid: [num] }, caption: `Goodbye Sis @${num.split("@")[0]}, I Hope You Feel At Home In Nature`,
+GpStore.sendMessage(anu.id, { image: { url: ppowner }, contextInfo: { mentionedJid: [num] }, caption: `Goodbye Sis @${num.split("@")[0]}, I Hope You Feel At Home In Nature`,
 jpegThumbnail: await reSize(ppowner, 200, 200), 
 contextInfo: {
 "mentionedJid": [num],
@@ -143,7 +143,7 @@ console.log(err)
 }
 })
 
-kris.decodeJid = (jid) => {
+GpStore.decodeJid = (jid) => {
 if (!jid) return jid
 if (/:\d+@/gi.test(jid)) {
 let decode = jidDecode(jid) || {}
@@ -151,44 +151,44 @@ return decode.user && decode.server && decode.user + '@' + decode.server || jid
 } else return jid
 }
 
-kris.ev.on('contacts.update', update => {
+GpStore.ev.on('contacts.update', update => {
 for (let contact of update) {
-let id = kris.decodeJid(contact.id)
+let id = GpStore.decodeJid(contact.id)
 if (store && store.contacts) store.contacts[id] = { id, name: contact.notify }
 }
 })
 
-kris.getName = (jid, withoutContact  = false) => {
-id = kris.decodeJid(jid)
-withoutContact = kris.withoutContact || withoutContact 
+GpStore.getName = (jid, withoutContact  = false) => {
+id = GpStore.decodeJid(jid)
+withoutContact = GpStore.withoutContact || withoutContact 
 let v
 if (id.endsWith("@g.us")) return new Promise(async (resolve) => {
 v = store.contacts[id] || {}
-if (!(v.name || v.subject)) v = kris.groupMetadata(id) || {}
+if (!(v.name || v.subject)) v = GpStore.groupMetadata(id) || {}
 resolve(v.name || v.subject || PhoneNumber('+' + id.replace('@s.whatsapp.net', '')).getNumber('international'))
 })
 else v = id === '0@s.whatsapp.net' ? {
 id,
 name: 'WhatsApp'
-} : id === kris.decodeJid(kris.user.id) ?
-kris.user :
+} : id === GpStore.decodeJid(GpStore.user.id) ?
+GpStore.user :
 (store.contacts[id] || {})
 return (withoutContact ? '' : v.name) || v.subject || v.verifiedName || PhoneNumber('+' + jid.replace('@s.whatsapp.net', '')).getNumber('international')
 }
 
-kris.parseMention = (text = '') => {
+GpStore.parseMention = (text = '') => {
 return [...text.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')
 }
 
-kris.sendContact = async (jid, kon, quoted = '', opts = {}) => {
+GpStore.sendContact = async (jid, kon, quoted = '', opts = {}) => {
 let list = []
 for (let i of kon) {
 list.push({
-displayName: await kris.getName(i + '@s.whatsapp.net'),
+displayName: await GpStore.getName(i + '@s.whatsapp.net'),
 vcard: `BEGIN:VCARD\n
 VERSION:3.0\n
-N:${await kris.getName(i + '@s.whatsapp.net')}\n
-FN:${await kris.getName(i + '@s.whatsapp.net')}\n
+N:${await GpStore.getName(i + '@s.whatsapp.net')}\n
+FN:${await GpStore.getName(i + '@s.whatsapp.net')}\n
 item1.TEL;waid=${i}:${i}\n
 item1.X-ABLabel:Ponsel\n
 item2.EMAIL;type=INTERNET:tesheroku123@gmail.com\n
@@ -200,11 +200,11 @@ item4.X-ABLabel:Region\n
 END:VCARD`
 })
 }
-kris.sendMessage(jid, { contacts: { displayName: `${list.length} Kontak`, contacts: list }, ...opts }, { quoted })
+GpStore.sendMessage(jid, { contacts: { displayName: `${list.length} Kontak`, contacts: list }, ...opts }, { quoted })
 }
 
-kris.setStatus = (status) => {
-kris.query({
+GpStore.setStatus = (status) => {
+GpStore.query({
 tag: 'iq',
 attrs: {
 to: '@s.whatsapp.net',
@@ -220,14 +220,14 @@ content: Buffer.from(status, 'utf-8')
 return status
 }
 
-kris.public = true
+GpStore.public = true
 
-kris.sendImage = async (jid, path, caption = '', quoted = '', options) => {
+GpStore.sendImage = async (jid, path, caption = '', quoted = '', options) => {
 let buffer = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
-return await kris.sendMessage(jid, { image: buffer, caption: caption, ...options }, { quoted })
+return await GpStore.sendMessage(jid, { image: buffer, caption: caption, ...options }, { quoted })
 }
 
-kris.sendImageAsSticker = async (jid, path, quoted, options = {}) => {
+GpStore.sendImageAsSticker = async (jid, path, quoted, options = {}) => {
 let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
 let buffer
 if (options && (options.packname || options.author)) {
@@ -235,11 +235,11 @@ buffer = await writeExifImg(buff, options)
 } else {
 buffer = await imageToWebp(buff)
 }
-await kris.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted })
+await GpStore.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted })
 return buffer
 }
 
-kris.sendVideoAsSticker = async (jid, path, quoted, options = {}) => {
+GpStore.sendVideoAsSticker = async (jid, path, quoted, options = {}) => {
 let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
 let buffer
 if (options && (options.packname || options.author)) {
@@ -247,11 +247,11 @@ buffer = await writeExifVid(buff, options)
 } else {
 buffer = await videoToWebp(buff)
 }
-await kris.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted })
+await GpStore.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted })
 return buffer
 }
 
-kris.copyNForward = async (jid, message, forceForward = false, options = {}) => {
+GpStore.copyNForward = async (jid, message, forceForward = false, options = {}) => {
 let vtype
 if (options.readViewOnce) {
 message.message = message.message && message.message.ephemeralMessage && message.message.ephemeralMessage.message ? message.message.ephemeralMessage.message : (message.message || undefined)
@@ -281,11 +281,11 @@ contextInfo: {
 }
 } : {})
 } : {})
-await kris.relayMessage(jid, waMessage.message, { messageId:  waMessage.key.id })
+await GpStore.relayMessage(jid, waMessage.message, { messageId:  waMessage.key.id })
 return waMessage
 }
 
-kris.downloadAndSaveMediaMessage = async (message, filename, attachExtension = true) => {
+GpStore.downloadAndSaveMediaMessage = async (message, filename, attachExtension = true) => {
 let quoted = message.msg ? message.msg : message
 let mime = (message.msg || message).mimetype || ''
 let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]
@@ -300,7 +300,7 @@ await fs.writeFileSync(trueFileName, buffer)
 return trueFileName
 }
 
-kris.downloadMediaMessage = async (message) => {
+GpStore.downloadMediaMessage = async (message) => {
 let mime = (message.msg || message).mimetype || ''
 let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]
 const stream = await downloadContentFromMessage(message, messageType)
@@ -311,7 +311,7 @@ buffer = Buffer.concat([buffer, chunk])
 return buffer
 }
 
-kris.getFile = async (PATH, save) => {
+GpStore.getFile = async (PATH, save) => {
 let res
 let data = Buffer.isBuffer(PATH) ? PATH : /^data:.*?\/.*?;base64,/i.test(PATH) ? Buffer.from(PATH.split`,`[1], 'base64') : /^https?:\/\//.test(PATH) ? await (res = await getBuffer(PATH)) : fs.existsSync(PATH) ? (filename = PATH, fs.readFileSync(PATH)) : typeof PATH === 'string' ? PATH : Buffer.alloc(0)
 let type = await FileType.fromBuffer(data) || {
@@ -326,8 +326,8 @@ size: await getSizeMedia(data),
 ...type,
 data}}
 
-kris.sendMedia = async (jid, path, fileName = '', caption = '', quoted = '', options = {}) => {
-let types = await kris.getFile(path, true)
+GpStore.sendMedia = async (jid, path, fileName = '', caption = '', quoted = '', options = {}) => {
+let types = await GpStore.getFile(path, true)
 let { mime, ext, res, data, filename } = types
 if (res && res.status !== 200 || file.length <= 65536) {
 try { throw { json: JSON.parse(file.toString()) } }
@@ -345,30 +345,30 @@ else if (/image/.test(mime)) type = 'image'
 else if (/video/.test(mime)) type = 'video'
 else if (/audio/.test(mime)) type = 'audio'
 else type = 'document'
-await kris.sendMessage(jid, { [type]: { url: pathFile }, caption, mimetype, fileName, ...options }, { quoted, ...options })
+await GpStore.sendMessage(jid, { [type]: { url: pathFile }, caption, mimetype, fileName, ...options }, { quoted, ...options })
 return fs.promises.unlink(pathFile)}
 
-kris.sendText = (jid, text, quoted = '', options) => kris.sendMessage(jid, { text: text, ...options }, { quoted })
+GpStore.sendText = (jid, text, quoted = '', options) => GpStore.sendMessage(jid, { text: text, ...options }, { quoted })
 
-kris.serializeM = (m) => smsg(kris, m, store)
+GpStore.serializeM = (m) => smsg(GpStore, m, store)
 
-kris.ev.on('connection.update', async (update) => {
+GpStore.ev.on('connection.update', async (update) => {
 const { connection, lastDisconnect } = update	
 if (connection === 'close') {
 let reason = new Boom(lastDisconnect?.error)?.output.statusCode
-if (reason === DisconnectReason.badSession) { console.log(`Bad Session File, Please Delete Session and Scan Again`); kris.logout(); }
-else if (reason === DisconnectReason.connectionClosed) { console.log("Connection closed, reconnecting...."); krisBot(); }
-else if (reason === DisconnectReason.connectionLost) { console.log("Connection Lost from Server, reconnecting..."); krisBot(); }
-else if (reason === DisconnectReason.connectionReplaced) { console.log("Connection Replaced, Another New Session Opened, Please Close Current Session First"); kris.logout(); }
-else if (reason === DisconnectReason.loggedOut) { console.log(`Device Logged Out, Please Scan Again And Run.`); kris.logout(); }
-else if (reason === DisconnectReason.restartRequired) { console.log("Restart Required, Restarting..."); krisBot(); }
-else if (reason === DisconnectReason.timedOut) { console.log("Connection TimedOut, Reconnecting..."); krisBot(); }
-else kris.end(`Unknown DisconnectReason: ${reason}|${connection}`)
+if (reason === DisconnectReason.badSession) { console.log(`Bad Session File, Please Delete Session and Scan Again`); GpStore.logout(); }
+else if (reason === DisconnectReason.connectionClosed) { console.log("Connection closed, reconnecting...."); GpStoreBot(); }
+else if (reason === DisconnectReason.connectionLost) { console.log("Connection Lost from Server, reconnecting..."); GpStoreBot(); }
+else if (reason === DisconnectReason.connectionReplaced) { console.log("Connection Replaced, Another New Session Opened, Please Close Current Session First"); GpStore.logout(); }
+else if (reason === DisconnectReason.loggedOut) { console.log(`Device Logged Out, Please Scan Again And Run.`); GpStore.logout(); }
+else if (reason === DisconnectReason.restartRequired) { console.log("Restart Required, Restarting..."); GpStoreBot(); }
+else if (reason === DisconnectReason.timedOut) { console.log("Connection TimedOut, Reconnecting..."); GpStoreBot(); }
+else GpStore.end(`Unknown DisconnectReason: ${reason}|${connection}`)
 }
 console.log('Connected...', update)
 })
 
-kris.sendButtonText = (jid, buttons = [], text, footer, quoted = '', options = {}) => {
+GpStore.sendButtonText = (jid, buttons = [], text, footer, quoted = '', options = {}) => {
 let buttonMessage = {
 text,
 footer,
@@ -376,11 +376,11 @@ buttons,
 headerType: 2,
 ...options
 }
-kris.sendMessage(jid, buttonMessage, { quoted, ...options })
+GpStore.sendMessage(jid, buttonMessage, { quoted, ...options })
 }
 
-kris.sendKatalog = async (jid , title = '' , desc = '', gam , options = {}) =>{
-let message = await prepareWAMessageMedia({ image: gam }, { upload: kris.waUploadToServer })
+GpStore.sendKatalog = async (jid , title = '' , desc = '', gam , options = {}) =>{
+let message = await prepareWAMessageMedia({ image: gam }, { upload: GpStore.waUploadToServer })
 const tod = generateWAMessageFromContent(jid,
 {"productMessage": {
 "product": {
@@ -397,10 +397,10 @@ const tod = generateWAMessageFromContent(jid,
 "businessOwnerJid": `6287705048235@s.whatsapp.net`
 }
 }, options)
-return kris.relayMessage(jid, tod.message, {messageId: tod.key.id})
+return GpStore.relayMessage(jid, tod.message, {messageId: tod.key.id})
 } 
 
-kris.send5ButLoc = async (jid , text = '' , footer = '', img, but = [], options = {}) =>{
+GpStore.send5ButLoc = async (jid , text = '' , footer = '', img, but = [], options = {}) =>{
 var template = generateWAMessageFromContent(jid, proto.Message.fromObject({
 templateMessage: {
 hydratedTemplate: {
@@ -412,10 +412,10 @@ hydratedTemplate: {
 }
 }
 }), options)
-kris.relayMessage(jid, template.message, { messageId: template.key.id })
+GpStore.relayMessage(jid, template.message, { messageId: template.key.id })
 }
 
-return kris
+return GpStore
 }
 
-krisBot()
+GpStoreBot()
